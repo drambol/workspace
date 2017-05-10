c59654f03be6ce18a5448051f445432bfd78e7fe
drambol <cf88ex@gmail.com>
2017-05-10 15:02:55 +0800

    Update Login.rb

--- CucumberRuby/MyTest/features/Step_definitions/Login.rb
+++ CucumberRuby/MyTest/features/Step_definitions/Login.rb
@@ -1,7 +1,11 @@
 # require_relative '../Page/TestPage.rb'
-
-Given(/^Go to Login and login with "(.*?)" and "(.*?)"$/) do |username, password|
+Given(/^I am on Login Page$/) do
 	@page = LoginPage.new
 	@page.load
+end		
+
+Then(/^I login with "(.*?)" and "(.*?)"$/) do |username, password|
+	#@page = LoginPage.new
+	#@page.load
 	@page.login_with(username, password)
 end
