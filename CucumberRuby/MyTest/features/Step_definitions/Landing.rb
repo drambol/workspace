1ec7e1dafaff6068f6a697dda421b49fe175b3ba
drambol <cf88ex@gmail.com>
2017-05-10 15:02:32 +0800

    Update Landing.rb

--- CucumberRuby/MyTest/features/Step_definitions/Landing.rb
+++ CucumberRuby/MyTest/features/Step_definitions/Landing.rb
@@ -1,8 +1,6 @@
-Then(/^I am on Landing Page$/) do
+Given(/^I am on Landing Page$/) do
 	@page = LandingPage.new
 	sleep 5
-	#expect(page).to be_displayed
-	#page.click_menu
 end
 
 Then(/^I click Menu$/) do
