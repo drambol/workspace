6ff4afa1eaedd7a409a56f59db18592a92622cc0
drambol <cf88ex@gmail.com>
2017-05-16 15:16:40 +0800

    Update Landing.rb

--- CucumberRuby/MyTest/features/Step_definitions/Landing.rb
+++ CucumberRuby/MyTest/features/Step_definitions/Landing.rb
@@ -1,6 +1,13 @@
 Given(/^I am on Landing Page$/) do
 	@page = LandingPage.new
-	sleep 5
+	#sleep 15
+	#expect(@page).to have_xpath("//a[@id='menu']")
+	#expect(@page).to have_selector(:xpath, "//a[@id='menu']")
+	#@page.wait_until_menu_visible
+	@page.wait_for_menu
+	#expect(@page).to have_menu
+	#expect(@page).to have_content("I Have")
+	#expect(@page).to have_selector(:xpath, "//script[@id='metamorph-151-start']")
 end
 
 Then(/^I click Menu$/) do
