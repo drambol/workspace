c956412b5bddb238536fb405767365af2840f072
drambol <cf88ex@gmail.com>
2017-05-16 15:17:12 +0800

    Update LandingPage.rb

--- CucumberRuby/MyTest/features/Page/LandingPage.rb
+++ CucumberRuby/MyTest/features/Page/LandingPage.rb
@@ -5,9 +5,12 @@ require 'site_prism'
 
 class LandingPage < SitePrism::Page
 
-  element :menu, "a[id='menu']"
+  #element :menu, "a[id='menu']"
+  element :menu, :xpath, "//a[@id='menu']"
  
-  def click_menu()
-    menu.click
+  def click_menu
+    #wait_until{ menu.visible? }
+  	#menu.click 
+  	execute_script("$(menu).click()")
   end
 end
