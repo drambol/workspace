37782e15104136576968ad71bfbf702bc94ea924
drambol <cf88ex@gmail.com>
2017-08-18 16:16:30 +0800

    Add files via upload

---
+++ drambolTest/src/test/TestEnum.java
@@ -0,0 +1,12 @@
+package test;
+
+import utility.tools.WeekDay;
+
+public class TestEnum {
+	public static void main(String args[]) {
+        for (WeekDay day : WeekDay.values()) {
+            System.out.println(day + "====>" + day.getDay());
+        }
+        WeekDay.printDay(5);
+    }
+}
