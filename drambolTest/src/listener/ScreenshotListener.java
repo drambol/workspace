380901435549187380a6ca6b29cfb4eb1e501c19
drambol <cf88ex@gmail.com>
2017-10-12 14:41:12 +0800

    Create ScreenshotListener.java

---
+++ drambolTest/src/listener/ScreenshotListener.java
@@ -0,0 +1,73 @@
+package listener;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import baseline.AutoContext;
+import baseline.MyDriver;
+import utility.dateTime.DateTime;
+import org.testng.Reporter;
+
+
+public class ScreenshotListener implements ITestListener{
+	
+    @Override
+    public void onTestFailure(ITestResult result) {
+    	System.out.println("***** Error "+result.getName()+" test has failed *****");
+    	takeScreenShot();
+    }
+    
+    public void takeScreenShot() {
+    	MyDriver myDriver = AutoContext.myWebDriverTL.get();
+    	File scrFile = ((TakesScreenshot) myDriver.getOriginalDriver()).getScreenshotAs(OutputType.FILE);
+    	String fileName = DateTime.getCurrentTime() + ".png";
+    	File desfile = new File(System.getProperty("user.dir") + "\\screenshot\\" + fileName);
+    	desfile.getParentFile().mkdirs();
+            try {
+            	FileUtils.copyFile(scrFile, desfile);
+				System.out.println("***Placed screen shot in "+System.getProperty("user.dir") + "\\screenshot\\" + fileName+" ***");
+				Reporter.log("screenshot path = "+ desfile.getAbsolutePath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    }
+	public void onFinish(ITestContext context) {}
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
