1eee7d43f8c506dc056409d6e967c18ad4049a35
drambol <cf88ex@gmail.com>
2017-10-13 11:00:48 +0800

    Create TestB.java

---
+++ drambolTest/src/test/selfTest/TestB.java
@@ -0,0 +1,39 @@
+package test.selfTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestB {
+
+	@Test
+	public void firstTestCase() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("im in first test case from ClassTwo Class");
+	}
+
+	@Test
+	public void secondTestCase() throws Exception {
+		Thread.sleep(500);
+		System.out.println("im in second test case from ClassTwo Class");
+		Assert.fail("Failing this Test");
+	}
+
+	@Test
+	public void thirdTestCase() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("im in third test case from ClassTwo Class");
+	}
+
+	@Test
+	public void fourthTestCase() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("im in fourth test case from ClassTwo Class");
+	}
+
+	@Test
+	public void fifthTestCase() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("im in fifth test case from ClassTwo Class");
+	}
+
+}
