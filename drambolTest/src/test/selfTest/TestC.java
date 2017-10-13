84d9c61c39dec94199da2364e0b28160c37197cc
drambol <cf88ex@gmail.com>
2017-10-13 11:01:06 +0800

    Create TestC.java

---
+++ drambolTest/src/test/selfTest/TestC.java
@@ -0,0 +1,20 @@
+package test.selfTest;
+
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class TestC {
+	@Test
+	public void firstTestCase() throws Exception {
+		Thread.sleep(100);
+		System.out.println("im in first test case from ClassThree Class");
+	}
+
+	@Test
+	public void secondTestCase() throws Exception {
+		Thread.sleep(5000);
+		System.out.println("im in second test case from ClassThree Class");
+		throw new SkipException("Skipping this test with exception");
+	}
+
+}
