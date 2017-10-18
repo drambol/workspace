1b5be75685fb862f54df0533b09d6dd5759b4c1f
drambol <cf88ex@gmail.com>
2017-10-18 18:00:30 +0800

    Update StartUp.java

--- CucumberJava/src/stepDefinition/StartUp.java
+++ CucumberJava/src/stepDefinition/StartUp.java
@@ -1,11 +1,16 @@
 package stepDefinition;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
@@ -23,7 +28,23 @@ public class StartUp {
 	}
 
 	@After
-	public void after() {
+	public void embedScreenshot(Scenario scenario) {
+		if (scenario.isFailed()) {
+			File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			String fileName = DateTime.getCurrentTime() + ".png";
+			File desfile = new File(System.getProperty("user.dir") + "\\screenshot\\" + fileName);
+			desfile.getParentFile().mkdirs();
+			try {
+				FileUtils.copyFile(scrFile, desfile);
+				System.out.println("***Placed screen shot in " + System.getProperty("user.dir") + "\\screenshot\\"
+						+ fileName + " ***");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		
 		driver.close();
 		driver.quit();
 	}
